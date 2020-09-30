55a406c4e23a93d9def15c5f90dc8e36b4fba5db
Nishkarsh Jain <51579623+nishkarshcode@users.noreply.github.com>
2020-10-01 00:17:26 +0530

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
-# images
+
+# Media Files (All media upload to this path)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'store'
